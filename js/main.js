2addf15cfb7e5cdb27875465becf6ff60bd683a8
Veysel Hadzhiveysal <veysel.hadzhiveysal@students.plymouth.ac.uk>
2026-01-21 21:00:38 +0000

    added basic leaflet map to geochronos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("GeoChronos initialised");
+// Initialise the map
+const map = L.map("map").setView([20, 0], 2);
+
+// Add OpenStreetMap tiles
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors"
+}).addTo(map);
+
+console.log("Map loaded");

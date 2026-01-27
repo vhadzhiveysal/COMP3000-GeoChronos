e86b9c02e5a7e572adf739463f8823e5d3714f61
Veysel Hadzhiveysal <veysel.hadzhiveysal@students.plymouth.ac.uk>
2026-01-27 15:15:35 +0000

    load events from json & display map markers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
-// Initialise the map
+// initialise the map
 const map = L.map("map").setView([20, 0], 2);
 
-// Add OpenStreetMap tiles
+// add OpenStreetMap tiles
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors"
 }).addTo(map);
 
-console.log("Map loaded");
+
+// load event data
+fetch("data/sample-events.json")
+  .then(response => response.json())
+  .then(events => {
+    console.log("Events loaded:", events);
+    displayEvents(events);
+  })
+  .catch(error => {
+    console.error("Error loading events:", error);
+  });
+
+
+// display markers on the map
+function displayEvents(events) {
+  events.forEach(event => {
+
+    const marker = L.marker([event.lat, event.lon]).addTo(map);
+
+    const popupContent = `
+      <strong>${event.title}</strong><br>
+      Year: ${event.year}<br>
+      ${event.summary}
+    `;
+
+    marker.bindPopup(popupContent);
+
+  });
+}

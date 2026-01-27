ff07577a723d5d4fce67260df963f199bab07697
Veysel Hadzhiveysal <veysel.hadzhiveysal@students.plymouth.ac.uk>
2026-01-27 15:52:54 +0000

    added timeline & ui improvements
    created timeline that updates automatically based on earliest and latest years of events in set
    updated layout with centred containers and padding

--- js/main.js
+++ js/main.js
@@ -10,35 +10,85 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // create cluster group
 const markerCluster = L.markerClusterGroup();
 
+// store all events
+let allEvents = [];
+
+// timeline events
+const slider = document.getElementById("slider");
+const rangeDisplay = document.getElementById("rangeDisplay");
+
 // load event data
 fetch("data/sample-events.json")
   .then(response => response.json())
   .then(events => {
-    console.log("Events loaded:", events);
-    displayEvents(events);
+    allEvents = events;
+
+    setupTimeline(allEvents);
+    renderEvents(allEvents);
   })
   .catch(error => {
     console.error("Error loading events:", error);
   });
 
-// display markers on the map (with clustering)
-function displayEvents(events) {
+// setup timeline
+function setupTimeline(events) {
+
+  const years = events.map(e => e.year);
+
+  const minYear = Math.min(...years);
+  const maxYear = Math.max(...years);
+
+  noUiSlider.create(slider, {
+    start: [minYear, maxYear],
+    connect: true,
+    step: 1,
+    range: {
+      min: minYear,
+      max: maxYear
+    },
+    format: {
+      to: value => Math.round(value),
+      from: value => Number(value)
+    }
+  });
+
+  slider.noUiSlider.on("update", handleTimelineChange);
+}
+
+
+// render markers
+function renderEvents(events) {
+
+  markerCluster.clearLayers();
+
   events.forEach(event => {
 
-    const marker = L.marker([event.lat, event.lon]).addTo(map);
+    const marker = L.marker([event.lat, event.lon]);
 
-    const popupContent = `
+    marker.bindPopup(`
       <strong>${event.title}</strong><br>
-      Year: ${event.year}<br>
+      ${event.year}<br>
       ${event.summary}
-    `;
+    `);
 
-    marker.bindPopup(popupContent);
-
-    // add to cluster
     markerCluster.addLayer(marker);
   });
 
-  // add cluster group to map
   map.addLayer(markerCluster);
+}
+
+
+// timeline handler
+function handleTimelineChange(values) {
+  const start = Number(values[0]);
+  const end = Number(values[1]);
+
+  rangeDisplay.textContent =
+   `Showing: ${start} - ${end}`;
+
+  const filtered = allEvents.filter(e =>
+    e.year >= start && e.year <= end
+  );
+
+  renderEvents(filtered);
 }
